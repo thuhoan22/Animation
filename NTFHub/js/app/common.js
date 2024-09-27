27d80bc582b73728c69df8acf4e5764c1c51489d
thuhoan22 <thuhoan2208@gmail.com>
2024-09-27 11:40:18 +0700

    update image and source code

--- NTFHub/js/app/common.js
+++ NTFHub/js/app/common.js
@@ -196,80 +196,40 @@
       }
     });
   }
-
-	function animateCountup(id, start, end, duration) {
-    if (start === end) return;
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-			current += increment;
-			obj.innerHTML = current;
-			if (current == end) {
-					clearInterval(timer);
-			}
-    }, stepTime);
-	}
-
-	const counterUpEv = $('.section-choose');
-	var animateCountupActive = function () {
-    var dataCountup = $('[data-count]');
-    dataCountup.each(function () {
-      var $this = $(this),
-        thisOffSetTop = $this.offset().top,
-        windowScroll = $(window).scrollTop(),
-        scrollPoint = windowScroll + $(window).innerHeight();
-			
-      if (thisOffSetTop <= scrollPoint) {
-				animateCountup('counter1', 250, 400, 800);
-				animateCountup('counter2', 0, 20, 800);
-				animateCountup('counter3', 50, 230, 800);
-				animateCountup('counter4', 0, 25, 800);
-      }
-    });
-  };
-
-	var counterUp = function () {
-		var counterElem = $('.counter');
-
-		counterElem.each(function () {
-			var $this = $(this),
-					countTo = $this.attr('data-count'),
-					ended = $this.attr('ended');
+	
+	var counterNumber = function () {
+		var thisOffSetTop = parseInt($('.choose-overview').offset().top),
+		    windowScroll = $(window).scrollTop(),
+		    windowHeight = $(window).innerHeight(),
+				value = 0;
+				
+		if (value == 0 && windowScroll  > thisOffSetTop - windowHeight) {
+			$('.counter').each(function() {
+				var $this = $(this),
+						countTo = $this.attr('data-count');
 					
-			// console.log(countNum);
-			
-			if (ended != 'true') {
-				$({countNum: $this.text()}).animate({
+				$this.prop('Counter', 0).animate({
 					countNum: countTo
-				}, {
-					duration: 1000,
+				},{
+					duration: 2000,
 					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						var resultNum = this.countNum;
-						if(resultNum > 999){
-							thisValComma = Number(resultNum).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-							$this.text(thisValComma);
-						} else {
-							$this.text(this.countNum);
+					step: function (now, tween) {
+						if(parseInt(tween.end) == parseFloat(tween.end)){
+							$this.text(Math.ceil(now));
+						} else{
+							$this.text(now.toFixed(1));
 						}
+					},
+					complete: function() {
+						$this.text(this.countNum);
 					}
-				});
-				$this.attr('ended', 'true');
-			}
-		});
-	}
-	if (!(counterUpEv.hasClass("is_active"))) {
-		counterUpEv.addClass("is_active");
-		// counterUp();
+				})
+			});
+
+			value = 1;
+		}
 	}
 
-	// ScrollMagic
 	var sceneMinting = new ScrollMagic.Scene({
     triggerElement: '#section-minting',
     triggerHook: 1.5,
@@ -293,7 +253,7 @@
 		parallaxImg();
 		horizontalScroll();
 		showTextSplit();
-		animateCountupActive();
+		counterNumber();
 	});
 
 	$(win).on('load', function () {
@@ -306,7 +266,7 @@
 		splitText();
 		setTransitionDelayLine();
 		showTextSplit();
-		animateCountupActive();
+		// counterNumber();
 	});
 
 	$(win).on('resize', function () {
